742263bab33500d8fa6c00c28dfbcf23b8ead1c7
P3dr0-M3l0 <pedromelo.oliveiras@gmail.com>
2026-05-28 15:40:09 -0300

    feat: add class Inventario with all initial implementations

---
+++ src/entidades/inventario.py
@@ -0,0 +1,42 @@
+class Inventario():
+    def __init__(self, capacidade_max, lista_itens, event_manager):
+        self.__capacidade_max = capacidade_max
+        self.__lista_itens = lista_itens
+        
+        
+    def definir_capacidade(self, valor):
+        if valor <= 0 or valor > 20:
+            raise Exception('''Erro: Não é possível criar um inventário com capacidade
+fora do intervalo  0 < x <= 20''')    
+        self.__capacidade_max = valor
+        
+    def adicionar_item(self, item):
+        if self.__capacidade_max == None:
+            raise ValueError("Erro: Não foi definida capacidade para esse inventário")
+        if len(self.__lista_itens) < self.__capacidade_max:
+            self.__lista_itens.append(item)
+            return True
+        return False
+    
+    def remover_item(self, item):
+        if item not in self.__lista_itens:
+            raise Exception("Erro: Item para ser removido não foi encontrado")
+        
+        self.__lista_itens.remove(item)
+        
+        if item in self.__lista_itens:
+            raise Exception("Erro: O item não foi removido corretamente do inventário")
+        
+        return item
+    
+    def serializar(self):
+        
+        itens_serializados = []
+        for item in self.__lista_itens:
+            itens_serializados.append(item.serializar())
+        
+        dicionario_inventario = {
+            'IN_capacidade_max': self.__capacidade_max,
+            'IN_lista_itens'   : itens_serializados
+        }
+        return dicionario_inventario
