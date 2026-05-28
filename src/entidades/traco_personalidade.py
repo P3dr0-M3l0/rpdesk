5dbf1db87c8f555e6cf1abd8e041272ca3919a03
P3dr0-M3l0 <pedromelo.oliveiras@gmail.com>
2026-05-28 19:07:31 -0300

    feat: add abstract class TracoPersonalidade

---
+++ src/entidades/traco_personalidade.py
@@ -0,0 +1,20 @@
+from abc import ABC, abstractmethod
+
+
+class TracoPersonalidade(ABC):
+    def __init__(self, nome, heroi_dono):
+        self._nome = nome
+        self._heroi_dono = heroi_dono
+
+
+    @abstractmethod
+    def inicializar_hooks(self, event_manager):
+        pass
+
+    @abstractmethod
+    def avaliar_situacao(self, contexto):
+        pass
+
+    @abstractmethod
+    def serializar(self):
+        pass
