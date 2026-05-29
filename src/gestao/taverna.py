d69920eb47448d95cac5464cbf53029518c4aae2
P3dr0-M3l0 <pedromelo.oliveiras@gmail.com>
2026-05-29 11:17:41 -0300

    feat: add class Taverna with all initial implementations

---
+++ src/gestao/taverna.py
@@ -0,0 +1,42 @@
+class Taverna:
+    def __init__(self, herois_disponiveis, fabrica_herois, event_manager):
+        self.__herois_disponiveis = herois_disponiveis
+        self.__fabrica_herois = fabrica_herois
+        self.__event_manager = event_manager
+
+        
+    def obter_vitrine(self):
+        return self.__herois_disponiveis
+        
+    def inicializar_hooks(self):
+        self.__event_manager.inscrever('novo_dia', self.__renovar_herois)    
+    
+    def remover_heroi_comprado(self, heroi):
+        if heroi not in self.__herois_disponiveis:
+            raise Exception("Erro: O herói a ser removido não existe")
+        self.__herois_disponiveis.remove(heroi)
+        if heroi in self.__herois_disponiveis:
+            raise Exception("Erro: O herói não foi corretamente removido da vitrine")
+        return heroi
+    
+    def __renovar_herois(self):
+        for i in range(len(self.__herois_disponiveis)):
+            self.__herois_disponiveis.pop(i)
+
+        if self.__herois_disponiveis != []:
+            raise Exception("Erro: Os heróis não foram removidos corretamente da vitrinni")        
+
+        # Por enquanto, a vitrine vai sempre ter 3 heróis
+        for i in range(3):
+            heroi = self.__fabrica_herois.gerar_heroi()
+            self.__herois_disponiveis.append(heroi)
+            
+    def serializar(self):
+        herois_serializados = []
+        for heroi in self.__herois_disponiveis:
+            herois_serializados.append(heroi.serializar())
+        
+        dicionario_taverna = {
+            'TA_herois_disponiveis' : herois_serializados
+        }
+        return dicionario_taverna
