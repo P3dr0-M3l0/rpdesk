04e7d54b59f142811a5a2f20b848610a32349361
P3dr0-M3l0 <pedromelo.oliveiras@gmail.com>
2026-05-31 18:22:28 -0300

    feat: add class Guilda with all initial implementations

---
+++ src/gestao/guilda.py
@@ -0,0 +1,73 @@
+from equipe import Equipe
+
+
+class Guilda():
+    def __init__(self, ouro, reputacao, roster_herois, equipes_ativas, inventario_guilda):
+        self.__ouro = ouro
+        self.__reputacao = reputacao
+        self.__roster_herois = roster_herois # lista de HEROIS disponiveis para EQUIPES
+        self.__equipes_ativas = equipes_ativas # lista de EQUIPES
+        self.__inventario_guilda = inventario_guilda # objeto do tipo Inventario
+
+
+    # ===============================================
+    # Gestão ----------------------------------------
+    # ===============================================
+    def contratar_heroi(self, heroi, custo):
+        if custo <= self.__ouro:
+            self.__ouro -= custo
+            self.__roster_herois.append(heroi)
+            return True
+        return False
+
+    def formar_equipe(self, nome, list_herois):
+        equipe = Equipe(
+            nome = nome,
+            membros = list_herois,
+            limite_membros = 4 # travei como 4 para todas
+        )
+        for heroi in list_herois:
+            self.__roster_herois.remove(heroi)
+        return equipe
+
+    # ===============================================
+    # Gestão do baú ---------------------------------
+    # ===============================================   
+    def adicionar_item_bau(self, item, heroi):
+        n_item = heroi.remover_item(item)
+        if not self.__inventario_guilda.adicionar_item(n_item):
+            heroi.adicionar_item(n_item)
+            print("O baú da guilda está cheio")
+            return False
+        return True
+
+    def apagar_item(self, item):
+        self.__inventario_guilda.remover_item(item)
+
+    def transferir_item_heroi(self, item, heroi):
+        n_item = self.__inventario_guilda.remover_item(item)
+        if not heroi.adicionar_item(n_item):
+            self.__inventario_guilda.adicionar_item(n_item)
+
+    # ===============================================
+    # Para save -------------------------------------
+    # ===============================================
+    def serializar(self):
+        # Serializando o roster
+        roster_serializado = []
+        for heroi in self.__roster_herois:
+            roster_serializado.append(heroi.serializar())
+
+        # Serializando as equipes
+        equipes_serializadas = []
+        for equipe in self.__equipes_ativas:
+            equipes_serializadas.append(equipe.serializar())
+
+        dicionario_guilda = {
+            'GU_ouro'             : self.__ouro,
+            'GU_reputacao'        : self.__reputacao,
+            'GU_roster_herois'    : roster_serializado,
+            'GU_equipes_ativas'   : equipes_serializadas,
+            'GU_inventario_guilda': self.__inventario_guilda.serializar()
+        }
+        return dicionario_guilda
