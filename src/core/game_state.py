f3776fbc18ae9c2097c3f91aa15c62bd113ffa1a
P3dr0-M3l0 <pedromelo.oliveiras@gmail.com>
2026-06-01 10:20:05 -0300

    feat: add getters and setters in class GameState

--- src/core/game_state.py
+++ src/core/game_state.py
@@ -7,14 +7,27 @@ class GameState():
         self.__marco_historia = marco_historia
         self.__list_missoes_concluidas = list_missoes_concluidas
     
-        
+
+    # =====================================================
+    # Getter e Setters ------------------------------------
+    # =====================================================        
     @property
     def dia_atual(self):
         return self.__dia_atual
     
+    @dia_atual.setter
+    def dia_atual(self, valor):
+        if valor <= 0:
+            raise ValueError("Erro: Dia inteiro não positivo não é permitido")
+        self.__dia_atual = valor
+    
     @property
     def guilda(self):
         return self.__guilda
+    
+    @property
+    def taverna(self):
+        return self.__taverna
     
     @property 
     def marco_historia(self):
@@ -24,11 +37,15 @@ class GameState():
     def list_missoes_concluidas(self):
         return self.__list_missoes_concluidas
     
-    
+    # =====================================================
+    # Gerenciamento do Dia---------------------------------
+    # =====================================================        
     def incrementar_dia(self):
         self.__dia_atual += 1
     
-        
+    # =====================================================
+    # Serialização e Desserialização-----------------------
+    # =====================================================        
     def get_estado_para_save(self):
         
         guilda_serializada = self.__guilda.serializar()
