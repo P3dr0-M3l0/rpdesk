bcfde3a05bdd5a6bd67b7cf4f7c2c6f228505234
P3dr0-M3l0 <pedromelo.oliveiras@gmail.com>
2026-06-06 18:18:16 -0300

    refactor: modify ConjuntoDeAtributos to use modifier list pattern

--- src/entidades/conjunto_atributos.py
+++ src/entidades/conjunto_atributos.py
@@ -12,46 +12,68 @@ class ConjuntoDeAtributos():
         self.__hp_atual = hp_max
         self.__event_manager = event_manager
 
-    # GETTERS -----------------------------------------------------------
+    # ===============================================
+    # GETTERS ---------------------------------------
+    # ===============================================
     
+    # Getters do Objeto Atributo
     # FORCA
     @property
     def forca(self):
         return self.__forca
-   
     # DESTREZA
     @property
     def destreza(self):
         return self.__destreza
-
     # INTELIGENCIA
     @property
     def inteligencia(self):
         return self.__inteligencia
-        
     # VELOCIDADE
     @property
     def velocidade(self):
         return self.__velocidade
-        
     # HP MAX
     @property
     def hp_max(self):
-        return self.__hp_max
-        
+        return self.__hp_max    
     # HP ATUAL
     @property
     def hp_atual(self):
         return self.__hp_atual
-
-    # --------------------------------------------------------------------------
-
-    def calcular_defesa_attr(self):
-        
-        # Verificar a defesa a partir dos atributos, como destreza
-        
-        ...
     
+    # Getters do valor numérico com modificações
+    # FORCA
+    @property
+    def valor_forca(self):
+        return self.__forca.valor_total
+    # DESTREZA
+    @property
+    def valor_destreza(self):
+        return self.__destreza.valor_total
+    # INTELIGENCIA
+    @property
+    def valor_inteligencia(self):
+        return self.__inteligencia.valor_total
+    # VELOCIDADE
+    @property
+    def valor_velocidade(self):
+        return self.__velocidade.valor_total
+    # HP MAX
+    @property
+    def valor_hp_max(self):
+        return self.__hp_max.valor_total
+    # HP ATUAL
+    @property
+    def valor_hp_atual(self):
+        return self.__hp_atual.valor_total
+
+    # ===============================================
+    # Matemática ------------------------------------
+    # ===============================================
+    def calcular_defesa_attr(self):
+        # Verificar a defesa a partir dos atributos, como destreza
+        ...
     
     def receber_dano(self, valor):
         
@@ -61,19 +83,25 @@ class ConjuntoDeAtributos():
             
         return self.hp_atual
     
-    def aplicar_buff_temporario(self, str_atributo, valor):
+    def aplicar_buff_temporario(self, str_atributo, modificador):
+        ...
+        
+    def remover_buff_temporario(self, str_atributo, modificador):
         ...
             
     def calcular_modificador(self, str_atributo):
         ...
 
+    # ===============================================
+    # Para save -------------------------------------
+    # ===============================================
     def serializar(self):
         dicionario_conjunto_attr = {
-            'AT_forca'        : self.__forca,
-            'AT_destreza'     : self.__destreza,
-            'AT_inteligencia' : self.__inteligencia,
-            'AT_velocidade'   : self.__velocidade,
-            'AT_hp_max'       : self.__hp_max,
-            'AT_hp_atual'     : self.__hp_atual
+            'AT_forca'        : self.__forca.valor_base,
+            'AT_destreza'     : self.__destreza.valor_base,
+            'AT_inteligencia' : self.__inteligencia.valor_base,
+            'AT_velocidade'   : self.__velocidade.valor_base,
+            'AT_hp_max'       : self.__hp_max.valor_base,
+            'AT_hp_atual'     : self.__hp_atual.valor_base
         }
         return dicionario_conjunto_attr
